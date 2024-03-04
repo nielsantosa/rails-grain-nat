4df1e907b3a93f7d43fb792774615154b40dea08
nielsantosa <nathaniel.santosa@gmail.com>
2024-03-05 01:26:09 +0800

    main: try

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -5,7 +5,7 @@ require_relative 'mutation/menu_section_mutation_type'
 require_relative 'mutation/section_mutation_type'
 require_relative 'mutation/section_item_mutation_type'
 require_relative 'mutation/item_mutation_type'
-require_relative 'mutation/img_mutation_type'
+#require_relative 'mutation/img_mutation_type'
 require_relative 'mutation/modifier_group_mutation_type'
 require_relative 'mutation/modifier_mutation_type'
 
